6504fa47f81b7222109313b59291ef411f0bca85
Bentura <avi.bentura@gmail.com>
2021-05-11 21:44:51 +0200

    Fermeture Modal d'inscription

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -1,23 +1,44 @@
-function editNav() {
-  var x = document.getElementById("myTopnav");
+// FONCTION BARRE DE NAVIGATION RESPONSIVE //
+
+const editNav = () => {
+  let x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
     x.className += " responsive";
   } else {
     x.className = "topnav";
   }
-}
+};
 
-// DOM Elements
+//*********************************** *//
+// ****** ELEMENTS DU DOM ************ //
+//***********************************  //
+
 const modalbg = document.querySelector(".bground");
+
+// Variables bouton "je m'inscrit"
 const modalBtn = document.querySelectorAll(".modal-btn");
+
 const formData = document.querySelectorAll(".formData");
 
-// launch modal event
+// Variables de fermeture de la modal
+const toClose = document.querySelectorAll(".btn-close");
+
+//******************************************* //
+// ************** ACTION *********************//
+//******************************************* //
+
+// Lancement de l'évenement de la modal
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+
+// Fermeture de la modal
+toClose.forEach((btn) => btn.addEventListener("click", closeModal));
 
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
 }
 
-
+// close modal form
+function closeModal() {
+  modalbg.style.display = "none";
+}
