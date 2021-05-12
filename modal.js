5ee917ebb509fab25764d9b711b182a1e539a627
Bentura <avi.bentura@gmail.com>
2021-05-13 00:07:14 +0200

    Vérification nombre de caractère - champs Nom

--- modal.js
+++ modal.js
@@ -65,9 +65,15 @@ const validateFirst = function (inputFirstName) {
   // Modification du texte sous le champs Prénom
   let smallFirst = inputFirstName.nextElementSibling;
 
-  // Au minimum 2 caracteres
+  // Au minimum 3 caracteres
   if (inputFirstName.value.length < 3) {
-    smallFirst.innerHTML = "Veuillez saisir au minimum 2 caractères";
+    smallFirst.innerHTML = "Veuillez saisir au minimum 3 caractères";
+    // Ajout de la class "danger"
+    smallFirst.classList.add("danger");
+    // Au maximum 16 caractères
+  } else if (inputFirstName.value.length >= 16) {
+    smallFirst.innerHTML = "Veuillez saisir un prénom plus court";
+    // Ajout de la class "danger"
     smallFirst.classList.add("danger");
   }
   // Testing de la valeur saisie dans le champs Prénom
@@ -89,4 +95,60 @@ form.last.addEventListener("change", function () {
 
 const validateLast = function (inputLastName) {
   //Création de la RegExp pour valider le Nom
+  let regexLast = new RegExp(
+    "^[a-zA-ZàâçéèêëìíîïñòóôöùúûüýÿÀÁÂÇÈÉÊËÌÍÎÏÑÒÓÔÙÚÛÜÝŸ]+$",
+    "g"
+  );
+
+  // Modification du texte sous le champs Nom
+  let smallLast = inputLastName.nextElementSibling;
+
+  // Au minimum 4 caracteres
+  if (inputLastName.value.length < 4) {
+    smallLast.innerHTML = "Veuillez saisir au minimum 4 caractères";
+    // Ajout de la class "danger"
+    smallLast.classList.add("danger");
+  } // Au maximum 16 caractères
+  else if (inputLastName.value.length >= 16) {
+    smallLast.innerHTML = "Veuillez saisir un nom plus court";
+    // Ajout de la class "danger"
+    smallLast.classList.add("danger");
+  }
+  // Testing de la valeur saisie dans le champs Prénom
+  else if (regexLast.test(inputLastName.value)) {
+    smallLast.classList.remove("danger");
+    smallLast.innerHTML = "";
+  } else {
+    smallLast.innerHTML = "Veuillez saisir votre nom";
+    smallLast.classList.add("danger");
+  }
+};
+
+//***************E-MAIL******************************//
+
+// Ecoute de la modification de l'élément E-mail
+form.email.addEventListener("change", function () {
+  validateEmail(this);
+});
+
+const validateEmail = function (inputEmail) {
+  //Création de la RegExp pour valider l'E-mail
+  let regexEmail = new RegExp(
+    "^[a-zA-Z0-9.-_]+[@]{1}[a-zA-Z0-9.-_]+[.]{1}[a-z]{2,10}$",
+    "g"
+  );
+
+  // Modification du texte sous le champs de saisie E-mail
+  let smallEmail = inputEmail.nextElementSibling;
+
+  // Testing de la valeur saisie dans le champs Prénom
+  if (regexEmail.test(inputEmail.value)) {
+    smallEmail.classList.remove("danger");
+    smallEmail.classList.add("success");
+    smallEmail.innerHTML = "Adresse e-mail valide";
+  } else {
+    smallEmail.classList.remove("success");
+    smallEmail.classList.add("danger");
+    smallEmail.innerHTML = "Adresse e-mail non valide";
+  }
 };
